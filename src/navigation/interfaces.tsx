45b321a2e2a00e5dbc645a4b0de32ad4b6cc7ef1
ALLYSSON CIDADE <120651526+AllyssonCidade@users.noreply.github.com>
2024-10-11 10:41:14 -0300

    Update interfaces.tsx

--- src/navigation/interfaces.tsx
+++ src/navigation/interfaces.tsx
@@ -5,8 +5,7 @@ export type RouteProps = {}
 
 //aqui devem ser colocadas e tipadas suas rotas com base no routeProps
 export type PropsStackRoutes = {
-  Dir: undefined | RouteProps
-  Settings: undefined | RouteProps
+  MeuComponente: undefined | RouteProps
 }
 
 export type PropsScreensApp = NativeStackScreenProps<PropsStackRoutes>
